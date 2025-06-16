6ae12a5a2fc7d61c96a0a7a1157d44019bd2d1dc
marlonocsan <marlon.val@srpro.marketing>
2025-06-16 20:37:59 +0800

    change welcome page as login page

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -13,14 +13,8 @@ defineProps<{
         <div class="w-full max-w-sm">
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
-                    <Link :href="route('home')" class="flex flex-col items-center gap-2 font-medium">
-                        <div class="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                            <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                        </div>
-                        <span class="sr-only">{{ title }}</span>
-                    </Link>
                     <div class="space-y-2 text-center">
-                        <h1 class="text-xl font-medium">{{ title }}</h1>
+                        <h2 class="text-3xl font-bold text-center text-white">{{ title }}</h2>
                         <p class="text-center text-sm text-muted-foreground">{{ description }}</p>
                     </div>
                 </div>
